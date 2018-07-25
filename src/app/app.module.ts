377665e127e2137da36b733af82a88368e663928
Josue Plaza <plazajosue2@gmail.com>
2018-07-25 16:57:04 -0500

    add http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { QuestionComponent } from './question/question.component';
-import { ResultComponent } from './result/result.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    QuestionComponent,
-    ResultComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
